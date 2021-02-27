c21619c96e982d143a50ba89add4c86c9d884c67
Irfan Fadilah <hello@irfanfadilah.com>
2021-02-28 06:46:36 +0700

    Add Service Worker

--- js/helpers.js
+++ js/helpers.js
@@ -1,3 +1,9 @@
+window.addEventListener("load", () => {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("service-worker.js");
+  }
+});
+
 function listOfYears() {
   step = (new Date).getFullYear() - 2020;
   return Array.from({ length: step }, (v, i) => 2021 + i);
